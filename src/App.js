1e600b486ae485a25043906a245374e9830cc582
Coby Yates <cobylyates@gmail.com>
2020-10-16 07:52:15 -0600

    Tasks displaying with chips, type working - Added type when entering todo item - Added background image - Todos full CRUD working - Some styling changed to an orange theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { TextField, Button } from "@material-ui/core";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import ToDo from "./ToDo";
 import db from "./firebase";
 import firebase from "firebase";
@@ -10,14 +12,15 @@ function App() {
   // when the app loads, fetch data from the db and put into todos
   useEffect(() => {
     db.collection("todos")
-      .orderBy("timestamp", "desc")
+      .orderBy("complete", "desc")
       .onSnapshot((snapshot) => {
         setTodos(
-          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo, complete: doc.data().complete, type: doc.data().type }))
         );
       });
   }, []);
   const [input, setInput] = useState("");
+  const [type, setType] = useState("");
   const addTodo = (event) => {
     // Prevents the form from refreshing the page
     event.preventDefault();
@@ -25,39 +28,67 @@ function App() {
     db.collection("todos").add({
       todo: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      complete: false,
+      type: type
     });
     // This will add the todo item to the db when the add btn is clicked
     setTodos([...todos, input]);
     setInput("");
+    setType("")
   };
+
+  const handleChange = (event) => {
+    setType(event.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="overlay">
+        <h1>The simple TODO app</h1>
+        <h2>Built in React & Material-UI</h2>
 
-      <form onSubmit={addTodo}>
-        <TextField
-          id="todo"
-          label="ToDo Item..."
-          variant="filled"
-          fullWidth
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
-        <Button
-          type="submit"
-          disabled={!input}
-          onClick={addTodo}
-          color="primary"
-          variant="contained"
-        >
-          Add
-        </Button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <ToDo todo={todo} />
-        ))}
-      </ul>
+        <form onSubmit={addTodo} className="app_inputs">
+          <TextField
+            id="todo"
+            autoFocus
+            label="ToDo Item..."
+            variant="filled"
+            value={input}
+            margin="dense"
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Select
+            value={type}
+            variant="filled"
+            margin="dense"
+            className="app_type"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"Personal"}>Personal</MenuItem>
+            <MenuItem value={"Business"}>Business</MenuItem>
+            <MenuItem value={"Other"}>Other</MenuItem>
+          </Select>
+          <Button
+            type="submit"
+            disabled={!type && !input}
+            onClick={addTodo}
+            color="primary"
+            variant="contained"
+          >
+            Add
+          </Button>
+        </form>
+        <div>
+          <ul className="list">
+            {todos.map((todo) => (
+              <ToDo key={todo.id} todo={todo} />
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/ToDo.js
+++ src/ToDo.js
@@ -8,8 +8,12 @@ import {
   Button,
   ListItemAvatar,
   ListItemText,
+  Chip,
+  Checkbox,
   TextField,
+  Divider,
   Modal,
+  FormControlLabel,
 } from "@material-ui/core";
 import "./ToDo.css";
 import { makeStyles } from "@material-ui/core/styles";
@@ -28,57 +32,85 @@ const useStyles = makeStyles((theme) => ({
 function ToDo(props) {
   const classes = useStyles();
   const [input, setInput] = useState(props.todo.todo);
+  let [checked, setCheck] = useState(props.todo.complete);
   const updateTodo = () => {
     // Update the todo
-      setOpen(false);
+    setOpen(false);
     db.collection("todos").doc(props.todo.id).set(
       {
         todo: input,
+        complete: checked,
       },
       { merge: true }
     );
   };
   const [open, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+  // const handleOpen = () => {
+  //   setOpen(true);
+  // };
+
+  function handleChange(evt) {
+    const value = evt.target.checked;
+    setCheck(() => (checked = value));
+    // const value =
+    //   evt.target.type === "checkbox" ? evt.target.checked : evt.target.value;
+    // setCheck({
+    //   ...checked,
+    //   [evt.target.name]: value,
+    // });
+    updateTodo();
+  }
+
   return (
     <>
-      <Modal open={open} onClose={(e) => setOpen(false)}>
-        <div className={classes.paper}>
-          <TextField
-            id="todo"
-            label="ToDo Item..."
-            variant="filled"
-            placeholder={props.todo.todo}
-            fullWidth
-            value={input}
-            onChange={(event) => setInput(event.target.value)}
-          />
-          <Button
-            color="secondary"
-            variant="contained"
-            onClick={updateTodo}
-          >
-            Update
-          </Button>
-        </div>
-      </Modal>
-      <List className="todo_list">
-        <ListItem>
-          {/* <ListItemAvatar>
-              </ListItemAvatar> */}
-          <ListItemText primary={props.todo.todo} secondary="Todo" />
+      <div>
+        <Modal
+          className="todo_modal"
+          open={open}
+          onClose={(e) => setOpen(false)}
+        >
+          <div className={classes.paper}>
+            <TextField
+              id="todo"
+              label="ToDo Item..."
+              variant="filled"
+              placeholder={props.todo.todo}
+              fullWidth
+              value={input}
+              onChange={(event) => setInput(event.target.value)}
+            />
+            <Button color="secondary" variant="contained" onClick={updateTodo}>
+              Update
+            </Button>
+          </div>
+        </Modal>
+      </div>
+      <List className="todo_list" key={props.todo.id}>
+        <ListItem key={props.todo.id} className="todo_list">
+          <form onSubmit={updateTodo} className="todo_checkbox">
+            <Checkbox
+              checked={props.todo.complete}
+              onChange={handleChange}
+              inputProps={{ "aria-label": "primary checkbox" }}
+            />
+          </form>
+          <div className="todo_type">
+            <ListItemText primary={props.todo.todo}  className="todo_text"/>
+            <Chip size="small" label={props.todo.type}  className="todo_chip"/>
+          </div>
+          <div className="todo_icons">
+            <EditIcon className="todo_icon" onClick={(e) => setOpen(true)} />
+            <DeleteIcon
+              onClick={(event) =>
+                db.collection("todos").doc(props.todo.id).delete()
+              }
+              color="secondary"
+              variant="contained"
+              className="todo_icon"
+            />
+          </div>
         </ListItem>
-        <EditIcon onClick={(e) => setOpen(true)} />
-        <DeleteIcon
-          onClick={(event) =>
-            db.collection("todos").doc(props.todo.id).delete()
-          }
-          color="secondary"
-          variant="contained"
-        />
       </List>
     </>
   );
